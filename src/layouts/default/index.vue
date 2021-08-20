f60b199398b6c4f2a051c6388f8c75f5e4a2238d
Igor Ferreira <vinicyosferreira@gmail.com>
2021-08-20 00:01:55 -0300

    Criação do layout default da aplicação

---
+++ src/layouts/default/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app id="app">
+    <v-app-bar app height="80">
+      <img
+        src="@/assets/img/pokebola.png"
+        alt="pokebola"
+        width="40"
+        height="40"
+        class="mr-md-4"
+      />
+      <v-toolbar-title
+        v-if="$vuetify.breakpoint.mdAndUp"
+        class="application-title"
+      >
+        Pokémon Generations
+      </v-toolbar-title>
+
+      <div class="page-title">
+        {{ $route.meta.title }}
+      </div>
+    </v-app-bar>
+
+    <v-main class="main-container px-5 px-md-16">
+      <router-view class="pt-5 pt-md-7" />
+    </v-main>
+  </v-app>
+</template>
+
+<script src="./script.js"></script>
+<style lang="scss" src="./style.scss" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: 'home',
+    component: () => import('@/layouts/default/index.vue'),
+    children: [
+      {
+        name: 'home',
+        path: '',
+        component: () => import('@/views/About.vue'),
+        meta: {
+          title: 'Página principal'
+        }
+      }
+    ]
   },
   {
     path: '/about',
