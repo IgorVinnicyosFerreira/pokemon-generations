94941c959c1d5322a51afa8d031061a2811a7361
Igor Ferreira <vinicyosferreira@gmail.com>
2021-08-21 01:21:16 -0300

    Componente item de lista

---
+++ src/components/list-item/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <img src="@/assets/img/pokebola.png" :alt="title" width="50px" />
+
+    <div class="title-container">
+      <p>{{ title }}</p>
+      <p v-show="subtitle">{{ subtitle }}</p>
+    </div>
+  </div>
+</template>
+<script src="./script.js"></script>
+<style lang="scss" src="./style.scss" scoped></style>
---
+++ src/components/list-item/script.js
@@ -0,0 +1,11 @@
+export default {
+  name: 'list-item',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    subtitle: String,
+    image: String
+  }
+}
